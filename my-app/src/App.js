83ae4d553569712d0fea7005755b166a71dfac18
MASA-JAPAN <masaifactoryline@gmail.com>
2019-10-15 20:44:21 +0900

    Create a judge logic

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Camera from "./components/camera.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Camera />
+    </div>
+  );
+}
+
+export default App;
